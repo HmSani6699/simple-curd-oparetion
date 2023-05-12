4749dd2d0a7ad47c6fad63c31672964b7c38ad3b
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-12 10:55:28 +0600

    added  data creat part

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const express = require('express');
 const cors = require('cors');
-const app =express();
-const port =process.env.PORT|| 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
 
+// meddelware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+// hmsadikurrahman120
+// rw7vRgnktk18ABE0
+
+const uri = "mongodb+srv://hmsadikurrahman120:rw7vRgnktk18ABE0@cluster0.fnxcgsn.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-app.get('/',(req,res)=>{
+        const usersCollection = client.db("usersBD").collection("users");
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log('New user', user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Hallo World')
 })
 
-app.listen(port,()=>{
-    console.log('SERVER IS RUNING  ON PORT ',port);
+app.listen(port, () => {
+    console.log('SERVER IS RUNING  ON PORT ', port);
 })
---
+++ step.js
@@ -0,0 +1,74 @@
+/***
+ *  1. [VISI] Mongodb website
+ *  2. login to google 
+ *  3. Accept privacy & thems of service   (submit)
+ * 
+ * 
+ *  4. Welcome page and three filth full up 
+ *  5. Deploy page : free host
+ * 
+ * 
+ *  6. Security Qickstart
+ * 
+ *       --------------
+ *      Open start manu and creat new server
+ *            --------------
+ *  1. mkdir simple-surd-oparation
+ *  2. cd simple-surd-oparation
+ *  3. npm i express cors mongodb
+ *  4. open package.josn and added to scripts: 'start':'node index.js
+ * 
+ *                -------------
+ *                     index.js 
+ *                    -----------------
+ * 
+ * 
+ *                        ------------
+ *                   Creat a simple client curd  site 
+ *                            -----------
+ *  1. set up to route  in rect router 
+ *  2. client site data to send in server site us to post method 
+ * 
+ * 
+ *           ----------------------
+ *                 data pas to server site 
+ *                   --------------------
+ * 
+ *  1. CLIENT SITE a (
+ * fetch('http://localhost:5000/user',{
+      method:'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+    })
+    )
+    * 2. SERVER SITE :  app.post('/user', async (req, res) => {
+         const user= req.body;
+         console.log('New user',user);
+        })
+    *
+    * 
+    *                        -----------------------
+    *                           DATABAGE DATA STOR and data creat
+    *                               ----------------------
+    * 
+    * 1. [VISIT ] :NODE MONGODB CURD >Usage Examples > Insert Operation >  Insert a Document
+    * 2.  const usersCollection = client.db("usersBD").collection("users");
+    * 3.  const result = await usersCollection.insertOne(user);
+    * 4.  res.send(result)
+    * 
+    * 
+    *               ---------------
+    *                     Data ret
+    *                  -------------
+    * 1. 
+    * 
+    * 
+    * 
+    * *
+ */
